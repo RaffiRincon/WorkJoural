e4410f455537cdc2c91c36558c589ba5be07cb41
Rafael Rincón <raffi.a.rincon@gmail.com>
2020-07-03 09:41:39 -0500

    Initial Commit

---
+++ Shared/Work_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_JournalApp.swift
+//  Shared
+//
+//  Created by Rafael Rincon on 7/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Work_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
