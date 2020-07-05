522dc822f7788966e3dfdb51f118fdffcbafa5e1
Rafael Rincón <raffi.a.rincon@gmail.com>
2020-07-05 03:11:09 -0500

    add files, gitignore

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var journalEntryStore: JournalEntryStore
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            List {
+                ForEach(journalEntryStore.journalEntries) { journalEntry in
+                    JournalEntryCell(journalEntry: journalEntry)
+                }
+                AddTaskStartCell()
+            }.navigationTitle("Tasks")
+        }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(journalEntryStore: TestData.journalEntries)
+        }
+    }
+}
+
+struct AddTaskStartCell: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                HStack() {
+                    Spacer()
+                    Image(systemName: "plus")
+                        .resizable(capInsets: EdgeInsets(), resizingMode: .stretch)
+                        .frame(width: 36, height: 36, alignment: .center)
+                        .colorScheme(.dark)
+                        .padding(.vertical, 8)
+                    Spacer()
+                }.background(Color(.systemBlue))
+            })
+            .cornerRadius(8)
+            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: nil, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }
+    }
+}
+
+struct JournalEntryCell: View {
+    @ObservedObject var journalEntry: JournalEntry
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            ZStack(alignment: .leading) {
+                Color(.darkGray)
+                Color(.white)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 2))
+                
+                HStack(alignment: .center, spacing: 0) {
+                    TextField("", text: $journalEntry.time.hoursMinutes)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 0))
+                        .frame(width: 50, alignment: .leading)
+                    TextField("", text: $journalEntry.time.amPM)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 0))
+                        .frame(width: 30, alignment: .leading)
+                }
+            }
+            ForEach(journalEntry.taskID, id: \.id) { component in
+                ZStack {
+                    Color(.darkGray)
+                    Color(.white)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 2))
+                    TextField("", text: component.$value)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 2))
+                        .frame(width: 25, height: 25, alignment: .center)
+                }
+            }
+        }
+        .fixedSize()
     }
 }
--- Shared/Work_JournalApp.swift
+++ Shared/Work_JournalApp.swift
@@ -6,12 +6,95 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct Work_JournalApp: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(journalEntryStore: TestData.journalEntries)
+        }.onChange(of: scenePhase) { newPhase in
+            if newPhase == .background {
+                
+            }
         }
     }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")	
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
 }
+
+
+
+class JournalEntryStore: Identifiable, ObservableObject {
+    let id = UUID()
+    @State var journalEntries: [JournalEntry]
+    
+    init(journalEntries: [JournalEntry] = []) {
+        self.journalEntries = journalEntries
+    }
+}
+
+class JournalEntry: Identifiable, ObservableObject {
+    let id = UUID()
+    var time: JournalEntryTime
+    var taskID: [TaskIDComponent]
+    
+    init(time: JournalEntryTime, taskID: [TaskIDComponent]) {
+        self.time = time
+        self.taskID = taskID
+    }
+}
+
+class TaskIDComponent: Identifiable, ObservableObject {
+    let id = UUID()
+    @State var value: String
+    
+    init(value: String) {
+        self.value = value
+    }
+}
+
+class JournalEntryTime: Identifiable, ObservableObject {
+    let id = UUID()
+    @State var hoursMinutes: String
+    @State var amPM: String
+    
+    init(hoursMinutes: String, amPM: String) {
+        self.hoursMinutes = hoursMinutes
+        self.amPM = amPM
+    }
+}
+
+
+struct TestData {
+    static var journalEntries = JournalEntryStore(journalEntries: {
+        var entries = [JournalEntry]()
+        entries.append(JournalEntry(time: .init(hoursMinutes: "00:00", amPM: "AM"), taskID: [.init(value: "0"), .init(value: "0")]))
+        let hourFormatter = DateFormatter()
+        hourFormatter.dateFormat = "h:mm"
+        let amPMFormatter = DateFormatter()
+        amPMFormatter.dateFormat = "a"
+        var time = Date()
+        
+        for i in 1 ... 5 {
+            for j in 1 ... Int.random(in: 2 ... 4) {
+                entries.append(JournalEntry(time: .init(hoursMinutes: hourFormatter.string(from: time), amPM: amPMFormatter.string(from: time)), taskID: [.init(value: "\(i)"), .init(value: "\(j)")]))
+                time = time.advanced(by: 60 * TimeInterval.random(in: 10...60))
+            }
+        }
+        
+        return entries
+    }())
+}
+
